9bce7742c237be3c39ee7d355ab6a527e7c0d29e
ThZedd <ber.cai.car@gmail.com>
2025-02-21 17:12:10 +0000

    Created Server

---
+++ Server/src/main/java/pt/iade/ei/DarkLeague/DarkLeagueApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.ei.DarkLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkLeagueApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/pt/iade/ei/DarkLeague/DarkLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.ei.DarkLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
